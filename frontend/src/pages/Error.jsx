009b185c1ff0dc8e541c8ced0860c0e51b44b7a4
Barbu Ionelia Alina <ionelia_alina.barbu@selir.com>
2023-10-18 00:11:24 +0300

    Handle error depending on the error status

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link, useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 
+// This page will be dispalyed every time we have an error in the app
 const ErrorPage = () => {
+  // Hook used to display a proper error message.
+  // The error object depends of the error type from the source
+  const error = useRouteError();
+  let errorTitle = "An error occurred";
+  let errorMessage = "Something went wrong";
+
+  if (error.status === 500) {
+
+    // throwing a json(method from eact-router-dom) error no need to parse the response
+    // errorMessage = JSON.parse(error.data).message;
+    errorMessage = error.data.message;
+  }
+
+  if (error.status === 404) {
+    errorTitle = "Not Found";
+    errorMessage = "Could not find the page";
+  }
   return (
     <>
-    <div>Errooooooooooooooooooooooooooor Page
-      <p> Return to <Link to='/'> Home</Link></p>
-    </div>
+      <MainNavigation />
+      <div>{errorTitle} </div>
+      <p>{errorMessage}</p>
+      <p>
+        Return to <Link to="/"> Home</Link>
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import RecipeList from "../components/RecipeList";
-import { useLoaderData } from "react-router-dom";
-
+import { json, useLoaderData } from "react-router-dom";
 
 const RecipesPage = () => {
   const recipesFromBE = useLoaderData();
@@ -11,14 +10,23 @@ const RecipesPage = () => {
 
 export default RecipesPage;
 
-
 // The loader code executes in the browser (user-side) not on the server
 export const recipeLoader = async () => {
   const response = await fetch("http://localhost:8080/recipes");
 
   if (!response.ok) {
-    throw new Response(
-      JSON.stringify({ message: "There are a problem in the recipe page" }),
+    // Here we can throw a object or a response
+    // Not posible throwing an error because we are in a async await f.
+
+    //- Option 1
+    // throw new Response(
+    //   JSON.stringify({ message: "There is a problem in the recipe page" }),
+    //   { status: 500 }
+    // );
+
+    //- Option 2 (less code)- no need to stringify or parse (json method will handle it)
+    throw json(
+      { message: "There is a problem in the recipe page" },
       { status: 500 }
     );
   } else {
